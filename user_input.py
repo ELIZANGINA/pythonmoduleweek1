d4e50698e150f525e3f16eeebb30dc480fe859ed
ELIZANGINA <nginae415@gmail.com>
2024-08-17 12:35:36 +0300

    my done assignment

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name = input("Enter your name : ")
+age = input("Enter your age : ")
+location = input (" What location do you stay : ")
+print ("Hello  " + name + ", you are  "  + str  (age ) +  "years  old and live in  " + location )
